ce8e0f7886c46c71e5224f4b9aa1c85926edfa84
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 00:03:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,7 +6,7 @@ def loop_message_five_times(message)
   end 
 end
 
-def loop_message_five_times(message, limit)
+def loop_message_n_times(message, limit)
   count = 0 
   while count < limit do
     puts message
